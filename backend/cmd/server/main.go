5622d0f4e4f71009037fe6239b6777c88762fc76
eskokado <eskokado@gmail.com>
2025-05-29 13:50:43 -0300

    feat: config main

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gopkg.in/gomail.v2"
+)
+
+func main() {
+	// Carregar variáveis de ambiente
+	_ = godotenv.Load(".env")
+	_ = gomail.NewDialer(
+		os.Getenv("SMTP_HOST"),
+		parsePort(os.Getenv("SMTP_PORT")),
+		os.Getenv("SMTP_USERNAME"),
+		os.Getenv("SMTP_PASSWORD"),
+	)
+
+	router := gin.Default()
+
+	router.Run(":8080")
+}
+
+func parsePort(port string) int {
+	var p int
+	fmt.Sscanf(port, "%d", &p)
+	return p
+}
