e9fce3772441378f0b7b3c2e3cffad76f0c598bf
eskokado <eskokado@gmail.com>
2025-05-29 13:53:34 -0300

    test: vo ID

---
+++ backend/tests/vo/id_test.go
@@ -0,0 +1,60 @@
+package vo
+
+import (
+	"errors"
+	"testing"
+
+	vo "github.com/eskokado/startup-auth-go/backend/pkg/domain/vos"
+	"github.com/eskokado/startup-auth-go/backend/pkg/msgerror"
+	"github.com/google/uuid"
+)
+
+func TestNewID(t *testing.T) {
+	id := vo.NewID()
+
+	if id.String() == "" {
+		t.Errorf("NewID() retornou um ID vazio")
+	}
+}
+
+func TestParseID(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		wantErr error
+	}{
+		{"Válido", uuid.New().String(), nil},
+		{"Vazio", "", msgerror.AnErrEmptyID},
+		{"Inválido Formato", "invalid-uuid", msgerror.AnErrInvalidID},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			id, err := vo.ParseID(tt.input)
+
+			if tt.wantErr != nil {
+				if err == nil || !errors.Is(err, tt.wantErr) {
+					t.Errorf("ParseID() erro = %v, esperado %v", err, tt.wantErr)
+				}
+				return
+			}
+
+			if id.String() != tt.input {
+				t.Errorf("ParseID() = %v, esperado %v", id.String(), tt.input)
+			}
+		})
+	}
+}
+
+func TestIDEqual(t *testing.T) {
+	id1 := vo.NewID()
+	id2 := vo.NewID()
+
+	if id1.Equal(id2) {
+		t.Errorf("IDEqual() falhou: IDs diferentes não devem ser iguais")
+	}
+
+	if !id1.Equal(id1) {
+		t.Errorf("IDEqual() falhou: O mesmo ID deve ser igual a si mesmo")
+	}
+}
