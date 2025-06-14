765df79eb08e38c498232ff7565c96aea0673bae
eskokado <eskokado@gmail.com>
2025-06-14 15:54:59 -0300

    fix: register dto

--- backend/pkg/dto/register_dto.go
+++ backend/pkg/dto/register_dto.go
@@ -1,16 +1,19 @@
 package dto
 
 type RegisterParams struct {
-	Name     string
-	Email    string
-	Password string
+	Name                 string
+	Email                string
+	Password             string
+	PasswordConfirmation string
+	ImageURL             string
 }
 
 type RegisterUserInput struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	ImageURL string `json:"image_url"`
+	Name                 string `json:"name"`
+	Email                string `json:"email"`
+	Password             string `json:"password"`
+	PasswordConfirmation string `json:"password_confirmation"`
+	ImageURL             string `json:"image_url"`
 }
 
 type RegisterUserOutput struct {
