1497f105100ad1744ed789d64c539f18c6a28f28
eskokado <eskokado@gmail.com>
2025-06-17 17:22:14 -0300

    refactor: impl msg error

--- backend/internal/usecase/auth/update_password_usecase.go
+++ backend/internal/usecase/auth/update_password_usecase.go
@@ -2,12 +2,16 @@ package usecase
 
 import (
 	"context"
-	"fmt"
+	"errors"
 
 	"github.com/eskokado/startup-auth-go/backend/pkg/domain/providers"
 	"github.com/eskokado/startup-auth-go/backend/pkg/domain/repository"
 	"github.com/eskokado/startup-auth-go/backend/pkg/domain/vo"
 	"github.com/eskokado/startup-auth-go/backend/pkg/msgerror"
+)
+
+var (
+	ErrSamePassword = errors.New("new password must be different")
 )
 
 type UpdatePasswordUseCase struct {
@@ -32,29 +36,54 @@ func (uc *UpdatePasswordUseCase) Execute(
 	newPassword string,
 ) error {
 	user, err := uc.userRepo.GetByID(ctx, userID)
+	if errors.Is(err, msgerror.AnErrNotFound) {
+		return msgerror.AnErrUserNotFound
+	}
 	if err != nil {
-		return fmt.Errorf("failed to get user: %w", err)
+		return msgerror.Wrap("failed to get user", err)
+	}
+	if user == nil {
+		return msgerror.AnErrUserNotFound
 	}
 
+	// Verify current password
 	match, err := uc.cryptoProvider.Compare(currentPassword, user.PasswordHash.String())
 	if err != nil {
-		return fmt.Errorf("failed to compare passwords: %w", err)
+		return msgerror.Wrap("failed to compare passwords", err)
 	}
 	if !match {
 		return msgerror.AnErrInvalidCredentials
 	}
 
+	// Check if new password is different
+	same, err := uc.cryptoProvider.Compare(newPassword, user.PasswordHash.String())
+	if err != nil {
+		return msgerror.Wrap("failed to verify password difference", err)
+	}
+	if same {
+		return ErrSamePassword
+	}
+
+	// Encrypt new password
 	newHash, err := uc.cryptoProvider.Encrypt(newPassword)
 	if err != nil {
-		return fmt.Errorf("failed to encrypt password: %w", err)
+		return msgerror.Wrap("failed to encrypt password", err)
 	}
 
 	newPasswordHash, err := vo.NewPasswordHash(newHash)
 	if err != nil {
-		return fmt.Errorf("invalid hash: %w", err)
+		return msgerror.Wrap("invalid hash", err)
 	}
 
-	updatedUser, _ := user.WithPasswordHash(newPasswordHash)
+	updatedUser, err := user.WithPasswordHash(newPasswordHash)
+	if err != nil {
+		return msgerror.Wrap("failed to update password", err)
+	}
+
 	_, err = uc.userRepo.Save(ctx, updatedUser)
-	return err
+	if err != nil {
+		return msgerror.Wrap("failed to save user", err)
+	}
+
+	return nil
 }
