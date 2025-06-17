c8da806a5be6ff097bd54176c48347f6ebf3886b
eskokado <eskokado@gmail.com>
2025-06-17 15:59:27 -0300

    test: update name usecase

--- backend/tests/internal/usecase/auth/update_name_usecase_test.go
+++ backend/tests/internal/usecase/auth/update_name_usecase_test.go
@@ -14,10 +14,18 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+var originalWithName = (*entity.User).WithName
+
 func TestUpdateNameUseCase_Execute(t *testing.T) {
 	ctx := context.Background()
 	validUserID := vo.NewID()
 	validUser := &entity.User{ID: validUserID}
+	oldName := "Existing Name"
+	oldNameVo, _ := vo.NewName(oldName, 3, 50)
+	userWithName := &entity.User{
+		ID:   validUserID,
+		Name: oldNameVo,
+	}
 
 	t.Run("Success", func(t *testing.T) {
 		mockRepo := new(mocks.MockUserRepo)
@@ -33,12 +41,35 @@ func TestUpdateNameUseCase_Execute(t *testing.T) {
 
 	t.Run("UserNotFound", func(t *testing.T) {
 		mockRepo := new(mocks.MockUserRepo)
-		mockRepo.On("GetByID", ctx, validUserID).Return(&entity.User{}, msgerror.AnErrUserNotFound)
+		mockRepo.On("GetByID", ctx, validUserID).Return((*entity.User)(nil), msgerror.AnErrNotFound)
+
+		uc := usecase.NewUpdateNameUseCase(mockRepo)
+		err := uc.Execute(ctx, validUserID, "New Name")
+
+		assert.ErrorIs(t, err, msgerror.AnErrUserNotFound)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("UnexpectedGetError", func(t *testing.T) {
+		mockRepo := new(mocks.MockUserRepo)
+		mockRepo.On("GetByID", ctx, validUserID).Return((*entity.User)(nil), errors.New("db connection failed"))
 
 		uc := usecase.NewUpdateNameUseCase(mockRepo)
 		err := uc.Execute(ctx, validUserID, "New Name")
 
 		assert.ErrorContains(t, err, "failed to get user")
+		assert.ErrorContains(t, err, "db connection failed")
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("NilUserAfterGet", func(t *testing.T) {
+		mockRepo := new(mocks.MockUserRepo)
+		mockRepo.On("GetByID", ctx, validUserID).Return((*entity.User)(nil), nil)
+
+		uc := usecase.NewUpdateNameUseCase(mockRepo)
+		err := uc.Execute(ctx, validUserID, "New Name")
+
+		assert.ErrorIs(t, err, msgerror.AnErrUserNotFound)
 		mockRepo.AssertExpectations(t)
 	})
 
@@ -49,40 +80,30 @@ func TestUpdateNameUseCase_Execute(t *testing.T) {
 		uc := usecase.NewUpdateNameUseCase(mockRepo)
 		err := uc.Execute(ctx, validUserID, "ab") // Nome muito curto
 
-		assert.ErrorIs(t, err, msgerror.AnErrInvalidUser)
+		assert.ErrorIs(t, err, msgerror.AnErrInvalidName)
 		mockRepo.AssertExpectations(t)
 	})
 
 	t.Run("SaveError", func(t *testing.T) {
 		mockRepo := new(mocks.MockUserRepo)
 		mockRepo.On("GetByID", ctx, validUserID).Return(validUser, nil)
-		mockRepo.On("Save", ctx, mock.Anything).Return(&entity.User{}, errors.New("save failed"))
+		mockRepo.On("Save", ctx, mock.Anything).Return((*entity.User)(nil), errors.New("save failed"))
 
 		uc := usecase.NewUpdateNameUseCase(mockRepo)
 		err := uc.Execute(ctx, validUserID, "Valid Name")
 
-		assert.ErrorContains(t, err, "save failed")
+		assert.ErrorContains(t, err, "failed to save user")
 		mockRepo.AssertExpectations(t)
 	})
 
 	t.Run("SameName", func(t *testing.T) {
-		oldName := "Existing Name"
-		oldNameVo, err := vo.NewName(oldName, 3, 50)
-		assert.NoError(t, err)
-		userWithName := &entity.User{
-			ID:   validUserID,
-			Name: oldNameVo,
-		}
-
 		mockRepo := new(mocks.MockUserRepo)
 		mockRepo.On("GetByID", ctx, validUserID).Return(userWithName, nil)
 
 		uc := usecase.NewUpdateNameUseCase(mockRepo)
-		err = uc.Execute(ctx, validUserID, oldName) // Mesmo nome atual
+		err := uc.Execute(ctx, validUserID, oldName) // Mesmo nome atual
 
-		assert.Error(t, err)
-		assert.ErrorContains(t, err, "new name must be different") // Mensagem específica
-
+		assert.ErrorIs(t, err, msgerror.AnErrNameDifferent)
 		mockRepo.AssertNotCalled(t, "Save")
 	})
 }
