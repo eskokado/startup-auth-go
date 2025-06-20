0dd98381ffc35fb638f1ea712281a959133b88f1
eskokado <eskokado@gmail.com>
2025-06-20 11:40:06 -0300

    fix: impl user profile component

--- frontend/layout/AppSidebar.tsx
+++ frontend/layout/AppSidebar.tsx
@@ -1,7 +1,15 @@
 import AppMenu from './AppMenu';
+import UserProfile from './UserProfile';
 
 const AppSidebar = () => {
-    return <AppMenu />;
+    return (
+        <div className="flex flex-column h-full">
+            <UserProfile />
+            <div className="flex-grow-1">
+                <AppMenu />
+            </div>
+        </div>
+    );
 };
 
 export default AppSidebar;
