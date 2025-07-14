291ceb1f75101a7ae11c80d53fa2fb2703a396bd
eskokado <eskokado@gmail.com>
2025-07-14 13:25:47 -0300

    test: add news methods

--- backend/tests/mocks/mock_blacklist_provider.go
+++ backend/tests/mocks/mock_blacklist_provider.go
@@ -20,3 +20,23 @@ func (m *MockBlacklist) Exists(ctx context.Context, token string) (bool, error) 
 	args := m.Called(ctx, token)
 	return args.Bool(0), args.Error(1)
 }
+
+func (m *MockBlacklist) ExistsKey(ctx context.Context, key string) (bool, error) {
+	args := m.Called(ctx, key)
+	return args.Bool(0), args.Error(1)
+}
+
+func (m *MockBlacklist) SetWithKey(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
+	args := m.Called(ctx, key, value, ttl)
+	return args.Error(0)
+}
+
+func (m *MockBlacklist) Get(ctx context.Context, key string) (string, error) {
+	args := m.Called(ctx, key)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockBlacklist) MGet(ctx context.Context, keys ...string) ([]interface{}, error) {
+	args := m.Called(ctx, keys)
+	return args.Get(0).([]interface{}), args.Error(1)
+}
