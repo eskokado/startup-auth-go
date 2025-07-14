7bb0d1719f833af487808e3f07c5499f56911eac
eskokado <eskokado@gmail.com>
2025-07-14 13:23:25 -0300

    feat: new methods redis blacklist provider

--- backend/internal/providers/redis_blacklist_provider.go
+++ backend/internal/providers/redis_blacklist_provider.go
@@ -9,6 +9,8 @@ import (
 
 // RedisCmdable define a interface mínima necessária para o blacklist
 type RedisCmdable interface {
+	Get(ctx context.Context, key string) *redis.StringCmd
+	MGet(ctx context.Context, keys ...string) *redis.SliceCmd
 	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) *redis.StatusCmd
 	Exists(ctx context.Context, keys ...string) *redis.IntCmd
 }
@@ -36,4 +38,37 @@ func (r *RedisBlacklist) Add(
 func (r *RedisBlacklist) Exists(ctx context.Context, token string) (bool, error) {
 	exists, err := r.client.Exists(ctx, "blacklist:"+token).Result()
 	return exists > 0, err
+}
+
+func (r *RedisBlacklist) ExistsKey(ctx context.Context, key string) (bool, error) {
+	exists, err := r.client.Exists(ctx, key).Result()
+	return exists > 0, err
+}
+
+func (r *RedisBlacklist) SetWithKey(
+	ctx context.Context,
+	key string,
+	value interface{},
+	ttl time.Duration,
+) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
+}
+
+func (r *RedisBlacklist) Get(
+	ctx context.Context,
+	key string,
+) (string, error) {
+	result, err := r.client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return "", nil
+	}
+	return result, err
+}
+
+func (r *RedisBlacklist) MGet(ctx context.Context, keys ...string) ([]interface{}, error) {
+	cmd := r.client.MGet(ctx, keys...)
+	if err := cmd.Err(); err != nil {
+		return nil, err
+	}
+	return cmd.Val(), nil
 }
